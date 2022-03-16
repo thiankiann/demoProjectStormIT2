49abfa49e6281b74b577b619bafc611274c7c7c2
@thiankiann <thiankiann@gmail.com>
2022-03-16 08:45:23 +0000

    First Endpoint

--- src/main/java/pl/stormit/demoProjectStormIT2/DemoProjectStormIt2Application.java
+++ src/main/java/pl/stormit/demoProjectStormIT2/DemoProjectStormIt2Application.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-
+@RestController
 public class DemoProjectStormIt2Application {
+	/**
+	 Exc.1 - simply app which use Get/@GetMapping and @RequestParam- adding variable
+	 */
+	@GetMapping("/hello")
+	public String hello(@RequestParam(name = "name", defaultValue = "<>")String name){
+		return String.format("Hello %s",name);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectStormIt2Application.class, args);

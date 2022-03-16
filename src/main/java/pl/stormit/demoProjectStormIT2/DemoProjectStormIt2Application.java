0327f4fdb5eb4c5bf295af197b16e5565303f59d
@thiankiann <thiankiann@gmail.com>
2022-03-16 08:04:44 +0000

    Initial Commit

---
+++ src/main/java/pl/stormit/demoProjectStormIT2/DemoProjectStormIt2Application.java
@@ -0,0 +1,13 @@
+package pl.stormit.demoProjectStormIT2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoProjectStormIt2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoProjectStormIt2Application.class, args);
+	}
+
+}
---
+++ src/test/java/pl/stormit/demoProjectStormIT2/DemoProjectStormIt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.stormit.demoProjectStormIT2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoProjectStormIt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
